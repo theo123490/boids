6bed293206b9a4f3a96ef6b2942404e102cf3b74
Theodore Gautama Chandra <theoengphys@gmail.com>
2020-12-24 23:16:24 +0700

    fine tuning boids equation

--- main/main.py
+++ main/main.py
@@ -3,35 +3,50 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
 birds = sim.Species("birds")
-birds.minimal_distance = 3
-birds.separation_constant = 0.2
-birds.alignment_constant = 0.03
-birds.cohesion_constant = 0.2
-birds.maximum_x = 30
-birds.maximum_y = 30
-birds.minimum_x = -30
-birds.minimum_y = -30
-birds.vision = 5
-birds.add_population(n_of_individual=20, random_range=[[10, 10], [15, 15]])
-birds.add_population(n_of_individual=5, random_range=[[5, 5], [0, 0]])
-birds.add_population(n_of_individual=20, random_range=[[-10, -10], [-5, -5]])
+birds.minimal_distance = 30
+birds.separation_constant = 30
+birds.alignment_constant = 0.5
+birds.cohesion_constant = 0.05
+birds.edge_force_constant = 4
+birds.bait_constant = 0.08
+birds.speed_limit = 30
+birds.maximum_x = 100
+birds.maximum_y = 400
+birds.minimum_x = -100
+birds.minimum_y = -400
+birds.vision = 40
+birds.add_population(n_of_individual=30, random_range=[[-100, 100], [-400, 400]], velocity_range=[[-100, 100], [-100, 100]])
 coordinates = birds.get_coordinates()
 
+span_x = birds.maximum_x - birds.minimum_x
+span_y = birds.maximum_y - birds.minimum_y
+
+# plt.figure(1)
+# scat = plt.scatter(coordinates[:, 0], coordinates[:, 1], s=30)
+# plt.xlim(birds.minimum_x - (span_x * 0.8), birds.maximum_x + (span_x * 0.8))
+# plt.ylim(birds.minimum_y - (span_y * 0.8), birds.maximum_y + (span_y * 0.8))
+#
+# birds.move()
+#
+# plt.figure(2)
+# scat = plt.scatter(coordinates[:, 0], coordinates[:, 1], s=30)
+# plt.xlim(birds.minimum_x - (span_x * 0.8), birds.maximum_x + (span_x * 0.8))
+# plt.ylim(birds.minimum_y - (span_y * 0.8), birds.maximum_y + (span_y * 0.8))
+
 fig, ax = plt.subplots()
-
-scat = plt.scatter(coordinates[:,0], coordinates[:,1], s=10)
-
+scat = plt.scatter(coordinates[:,0], coordinates[:,1], s=3)
+extra_border = 0.4
 def update_plot(scat):
     fig.clf()
     birds.move()
     coordinates = birds.get_coordinates()
-    scat = plt.scatter(coordinates[:, 0], coordinates[:, 1], s=10)
-    plt.xlim(birds.minimum_x, birds.maximum_x)
-    plt.ylim(birds.minimum_y, birds.maximum_y)
+    scat = plt.scatter(coordinates[:, 0], coordinates[:, 1], s=30)
+    plt.xlim(birds.minimum_x - (span_x * extra_border), birds.maximum_x + (span_x * extra_border))
+    plt.ylim(birds.minimum_y - (span_y * extra_border), birds.maximum_y + (span_y * extra_border))
 
     return scat
 
-ani = FuncAnimation(fig, update_plot, interval=30)
+ani = FuncAnimation(fig, update_plot, interval=50)
 
 plt.grid()
 plt.show()
--- main/simulation.py
+++ main/simulation.py
@@ -2,18 +2,21 @@ import numpy as np
 import sys
 
 class Individual:
-    def __init__(self, initial_x, initial_y, species, index, minimal_distance=0):
+    def __init__(self, initial_x, initial_y, species, index):
         self.x = initial_x
         self.y = initial_y
         self.species = species
-        self.minimal_distance = minimal_distance
         self.index = index
-        self.separation_velocity = [0,0]
-        self.x_velocity = 0
-        self.y_velocity = 0
-        self.alignment_velocity = 0
+        self.x_velocity = 30
+        self.y_velocity = 20
         self.center = [0,0]
-        self.center_velocity = [0,0]
+        self.separation_velocity = [0,0]
+        self.alignment_velocity = 0
+        self.coherence_velocity = [0, 0]
+        self.bait_velocity = [0,0]
+        self.edge_force = [0,0]
+        self.edge_force_constant = 1
+
 
     def set_species(self, species):
         self.species = species
@@ -21,19 +24,6 @@ class Individual:
     def move(self, move_x, move_y):
         self.x += move_x
         self.y += move_y
-
-        if self.x > self.species.maximum_x:
-            self.x = self.species.maximum_x - np.abs(move_x)
-            self.x_velocity = -1 * self.x_velocity
-        elif self.y > self.species.maximum_y:
-            self.y = self.species.maximum_y - np.abs(move_y)
-            self.y_velocity = -1 * self.y_velocity
-        elif self.x < self.species.minimum_x:
-            self.x = self.species.minimum_x + np.abs(move_x)
-            self.x_velocity = -1 * self.x_velocity
-        elif self.y < self.species.minimum_y:
-            self.y = self.species.minimum_y + np.abs(move_y)
-            self.y_velocity = -1 * self.y_velocity
 
     def location(self):
         print("current location is in x: {} y: {}".format(self.x, self.y))
@@ -44,14 +34,19 @@ class Individual:
     def calculate_individual_separation(self, other_individual):
         separation = [0,0]
         distance = self.calculate_distance(other_individual.x, other_individual.y)
-        if (distance < self.minimal_distance):
+        print("distance: {}".format(distance))
+        print("self.species.minimal_distance: {}".format(self.species.minimal_distance))
+        if (distance < self.species.minimal_distance):
             distance_x = other_individual.x - self.x
             distance_y = other_individual.y - self.y
-            distance_radian = np.arctan2(distance_y, distance_x)
-            separation_velocity = self.minimal_distance - distance
-            separation_x = np.cos(distance_radian) * separation_velocity * -1
-            separation_y = np.sin(distance_radian) * separation_velocity * -1
+            distance_radian = np.arctan2(distance_y, distance_x) + np.pi
+            separation_velocity = self.species.minimal_distance - distance
+            separation_x = np.cos(distance_radian) * separation_velocity
+            separation_y = np.sin(distance_radian) * separation_velocity
             separation = [separation_x, separation_y]
+            print("self {}".format([self.x, self.y]))
+            print("others {}".format([other_individual.x, other_individual.y]))
+            print("separation: {}".format(separation))
 
         return separation
 
@@ -59,48 +54,98 @@ class Individual:
         distance = self.calculate_distance(other_individual.x, other_individual.y)
         return distance < self.species.vision
 
-    def calculate_center(self):
+    def calculate_cohesion(self):
         other_x = []
         other_y = []
         for i in self.species.group:
             if self.is_visible(i):
-                if (i.index != self.index):
-                    other_x.append(i.x)
-                    other_y.append(i.y)
+                other_x.append(i.x)
+                other_y.append(i.y)
 
-        self.center = [np.mean(other_x), np.mean(other_y)]
+        self.center = [np.nan_to_num(np.mean(other_x)), np.nan_to_num(np.mean(other_y))]
 
     def calculate_separation_velocity(self):
         separation_vectors_x = []
         separation_vectors_y = []
         for i in self.species.group:
-            if self.is_visible(i):
-                if (i.index != self.index):
-                    individual_separation = self.calculate_individual_separation(i)
-                    separation_vectors_x.append(individual_separation[0])
-                    separation_vectors_y.append(individual_separation[1])
-        self.separation_velocity = np.array([np.mean(separation_vectors_x), np.mean(separation_vectors_y)]) * self.species.separation_constant
+            if (i.index != self.index):
+                individual_separation = self.calculate_individual_separation(i)
+                separation_vectors_x.append(individual_separation[0])
+                separation_vectors_y.append(individual_separation[1])
+
+        separation_x = np.nan_to_num(np.mean(separation_vectors_x)) * self.species.separation_constant
+        separation_y = np.nan_to_num(np.mean(separation_vectors_y)) * self.species.separation_constant
+        self.separation_velocity = [separation_x, separation_y]
 
     def calculate_alignment_velocity(self):
         aligment_velocities_x = []
         aligment_velocities_y = []
         for i in self.species.group:
-            if (i.index != self.index):
-                aligment_velocities_x.append(i.x)
-                aligment_velocities_y.append(i.y)
+            if self.is_visible(i):
+                if (i.index != self.index):
+                    aligment_velocities_x.append(i.x_velocity)
+                    aligment_velocities_y.append(i.y_velocity)
 
-        self.alignment_velocity = np.array([np.mean(aligment_velocities_x), np.mean(aligment_velocities_y)]) * self.species.alignment_constant
+        x_alignment_v = np.nan_to_num(np.mean(aligment_velocities_x)) * self.species.alignment_constant
+        y_alignment_v = np.nan_to_num(np.mean(aligment_velocities_y)) * self.species.alignment_constant
+
+        self.alignment_velocity = [x_alignment_v, y_alignment_v]
 
     def calculate_coherence_velocity(self):
-        self.calculate_center()
-        self.center_velocity = np.array([self.center[0] - self.x, self.center[1] - self.y]) * self.species.cohesion_constant
+        self.calculate_cohesion()
+        self.coherence_velocity = np.array([self.center[0] - self.x, self.center[1] - self.y]) * self.species.cohesion_constant
+
+    def calculate_speed_limit(self):
+        speed = np.sqrt(self.x_velocity**2 + self.y_velocity**2)
+        if speed>self.species.speed_limit :
+            self.x_velocity = (self.x_velocity/speed) * self.species.speed_limit
+            self.y_velocity = (self.y_velocity/speed) * self.species.\
+                speed_limit
+
+    def calculate_force(self, current, maximum):
+        excess = (np.abs(maximum-current))
+        if (excess > 0):
+            return np.abs(maximum-current) * self.edge_force_constant
+        else:
+            return 0
+
+    def calculate_edge_force(self):
+        edge_force_arr = [0, 0]
+        if self.x > self.species.maximum_x:
+            edge_force_arr[0] = -1 * self.calculate_force(self.x, self.species.maximum_x)
+            print(self.x)
+        elif self.x < self.species.minimum_x:
+            edge_force_arr[0] = 1 * self.calculate_force(self.x, self.species.minimum_x)
+            print(self.x)
+
+        if self.y > self.species.maximum_y:
+            edge_force_arr[1] = -1 * self.calculate_force(self.y, self.species.maximum_y)
+            print(self.y)
+        elif self.y < self.species.minimum_y:
+            edge_force_arr[1] = self.calculate_force(self.y, self.species.minimum_y)
+            print(self.y)
+
+        self.edge_force = edge_force_arr
+
+    def calculate_bait(self):
+        self.bait_velocity = np.array([0 - self.x, 0 - self.y]) * self.species.bait_constant
 
     def calculate_next_move(self):
         self.calculate_separation_velocity()
         self.calculate_alignment_velocity()
         self.calculate_coherence_velocity()
-        self.x_velocity = self.separation_velocity[0] + self.alignment_velocity[0] + self.center_velocity[0]
-        self.y_velocity = self.separation_velocity[1] + self.alignment_velocity[1] + self.center_velocity[1]
+        self.calculate_edge_force()
+        self.calculate_bait()
+        self.x_velocity = self.x_velocity + self.separation_velocity[0] + self.alignment_velocity[0] + self.coherence_velocity[0] + self.edge_force[0] + self.bait_velocity[0]
+        self.y_velocity = self.y_velocity + self.separation_velocity[1] + self.alignment_velocity[1] + self.coherence_velocity[1] + self.edge_force[1] + self.bait_velocity[1]
+        self.calculate_speed_limit()
+        print("current ID: {}".format(self.index))
+        print("current location x: {}, y:{}".format(self.x, self.y))
+        print("calculated velocity x: {}, y:{}".format(self.x_velocity, self.y_velocity))
+        print("calculate_separation_velocity {} ".format(self.separation_velocity))
+        print("calculate_alignment_velocity {} ".format(self.alignment_velocity))
+        print("calculate_coherenece_veolcity {}".format(self.coherence_velocity))
+        print("calculate_edge_force {}".format(self.edge_force))
 
     def next_frame(self):
         self.move(self.x_velocity, self.y_velocity)
@@ -119,17 +164,25 @@ class Species:
         self.separation_constant = 1
         self.alignment_constant = 1
         self.cohesion_constant = 1
+        self.speed_limit = 30
+        self.bait_constant = 0
 
-
-    def add_population(self, n_of_individual, random_range):
+    def add_population(self, n_of_individual, random_range, velocity_range = [[0,0],[0,0]]):
         x_min = random_range[0][0]
-        y_min = random_range[0][1]
-        x_max = random_range[1][0]
+        x_max = random_range[0][1]
+        y_min = random_range[1][0]
         y_max = random_range[1][1]
         x_coordinates = np.random.uniform(low=x_min, high=x_max, size=n_of_individual)
         y_coordinates = np.random.uniform(low=y_min, high=y_max, size=n_of_individual)
+
+        v_x_min = velocity_range[0][0]
+        v_x_max = velocity_range[0][1]
+        v_y_min = velocity_range[1][0]
+        v_y_max = velocity_range[1][1]
         for n in range(n_of_individual):
-            individual = Individual(initial_x=x_coordinates[n], initial_y=y_coordinates[n], species=self, index=np.random.randint(-sys.maxsize, sys.maxsize), minimal_distance=self.minimal_distance)
+            individual = Individual(initial_x=x_coordinates[n], initial_y=y_coordinates[n], species=self, index=np.random.randint(-sys.maxsize, sys.maxsize))
+            individual.x_velocity = np.random.uniform(v_x_min, v_x_max)
+            individual.y_velocity = np.random.uniform(v_y_min, v_y_max)
             self.group.append(individual)
 
     def get_coordinates(self):
