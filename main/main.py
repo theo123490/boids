336aaf2ecb7c591d9108b4a127f16fe3347efa9b
Theodore Gautama Chandra <theoengphys@gmail.com>
2020-12-24 09:35:54 +0700

    add separation feature

--- main/main.py
+++ main/main.py
@@ -3,7 +3,22 @@ import matplotlib.pyplot as plt
 
 
 birds = sim.Species("birds")
-birds.populate(30, [[ -5, -5], [5, 5]])
+birds.populate(n_of_individual=20, random_range=[[ -5, -5], [5, 5]], separation_constant=5)
+
+plt.figure(1)
 coordinates = birds.get_coordinates()
 plt.scatter(coordinates[:,0], coordinates[:,1])
+
+birds.move()
+
+plt.figure(2)
+coordinates_a = birds.get_coordinates()
+plt.scatter(coordinates_a[:,0], coordinates_a[:,1])
+
+birds.move()
+
+plt.figure(3)
+coordinates_b = birds.get_coordinates()
+plt.scatter(coordinates_b[:,0], coordinates_b[:,1])
+
 plt.show()
--- main/simulation.py
+++ main/simulation.py
@@ -1,11 +1,17 @@
 import numpy as np
 
 class Individual:
-    def __init__(self, initial_x, initial_y, species):
+    def __init__(self, initial_x, initial_y, species, index, separation_constant=0):
         self.x = initial_x
         self.y = initial_y
         self.species = species
-        self.separation = 0
+        self.separation_constant = separation_constant
+        self.index = index
+        self.separation_force = [0,0]
+        self.group = None
+
+    def set_group(self, group):
+        self.group = group
 
     def move(self, move_x, move_y):
         self.x += move_x
@@ -14,26 +20,59 @@ class Individual:
     def location(self):
         print("current location is in x: {} y: {}".format(self.x, self.y))
 
-    def calculate_separation(self, separation_constant, group):
-        group_x = group[:,0]
-        group_y = group[:,1]
-        self.separation = [np.mean(group_x), np.mean(group_y)]
+    def calculate_distance(self, x, y):
+        return np.sqrt(np.abs(x - self.x) ** 2 + np.abs(y - self.y) ** 2)
+
+    def calculate_individual_separation(self, other_individual):
+        separation = [0,0]
+        distance = self.calculate_distance(other_individual.x, other_individual.y)
+        if (distance < self.separation_constant):
+            separation_x = self.x - other_individual.x
+            separation_y = self.y - other_individual.y
+            separation = [separation_x, separation_y]
+
+        return separation
+
+    def calculate_separation_force(self):
+        separation_vectors_x = []
+        separation_vectors_y = []
+        for i in self.group:
+            if (i.index == self.index):
+                pass
+            else:
+                individual_separation = self.calculate_individual_separation(i)
+                separation_vectors_x.append(individual_separation[0])
+                separation_vectors_y.append(individual_separation[1])
+        self.separation_force = [np.mean(separation_vectors_x), np.mean(separation_vectors_y)]
+
+    def next_frame(self):
+        self.calculate_separation_force()
+        x_force = self.separation_force[0]
+        y_force = self.separation_force[1]
+        self.move(x_force, y_force)
+
+
 
 class Species:
     def __init__(self, species_name):
         self.species_name = species_name
         self.group = []
         self.center = None
+        self.separation = 0
 
-    def populate(self, n_of_individual, random_range):
+    def populate(self, n_of_individual, random_range, separation_constant=0):
         x_min = random_range[0][0]
         y_min = random_range[0][1]
         x_max = random_range[1][0]
         y_max = random_range[1][1]
+        self.separation = separation_constant
         x_coordinates = np.random.uniform(low=x_min, high=x_max, size=n_of_individual)
         y_coordinates = np.random.uniform(low=y_min, high=y_max, size=n_of_individual)
         for n in range(n_of_individual):
-            self.group.append(Individual(initial_x=x_coordinates[n], initial_y=y_coordinates[n], species=self.species_name))
+            individual = Individual(initial_x=x_coordinates[n], initial_y=y_coordinates[n], species=self.species_name, index=n, separation_constant=self.separation)
+            individual.group = self.group
+            self.group.append(individual)
+
 
     def get_coordinates(self):
         coordinates = []
@@ -41,3 +80,7 @@ class Species:
             coordinates.append([individual.x, individual.y])
         coordinates = np.array(coordinates)
         return coordinates
+
+    def move(self):
+        for individual in self.group:
+            individual.next_frame()
